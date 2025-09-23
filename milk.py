4263e3cca34763087d65813643239ad000a16303
Erik van Zijst <erik.van.zijst@gmail.com>
2025-09-23 22:56:46 +0200

    Row attribution

--- milk.py
+++ milk.py
@@ -1,3 +1,4 @@
+import os
 from datetime import date
 
 import pandas as pd
@@ -22,33 +23,6 @@ pills = st.container()
 save = st.container()
 table = st.container()
 
-def on_rows_change(session):
-    # {'edited_rows': {}, 'added_rows': [{'selected': True, 'item': 'asfdsafasdfas'}], 'deleted_rows': []}
-    if st.session_state['editor_df']['added_rows']:
-        rows = [{
-            'selected': 1 if r.get('selected') else 0,
-            'item': str(r.get('item')),
-            'created_on': today
-        } for r in st.session_state['editor_df']['added_rows']]
-        session.execute(
-            text('INSERT INTO milk (created_on, selected, item) VALUES (:created_on, :selected, :item)'),
-            rows)
-
-    for rid, props in st.session_state['editor_df']['edited_rows'].items():
-        rid = int(items.iloc[rid]['id'])
-        for k, v in props.items():
-            if k == 'selected':
-                v = 1 if v else 0  # in sqlite, boolean is 0/1
-            session.execute(
-                text(f'UPDATE milk set {k} = :v WHERE id = :id'), {'id': rid, 'v': v})
-
-    for idx in st.session_state['editor_df']['deleted_rows']:
-        rid = int(items.iloc[idx]['id'])
-        session.execute(text('DELETE FROM milk WHERE id = :id'), {'id': rid})
-
-    session.commit()
-
-
 today = date.today()
 conn = st.connection('milk', type='sql', ttl=0)
 st.session_state.setdefault('mode', 'View')
@@ -63,13 +37,13 @@ with pills:
     edit = st.pills(' ', ['View', 'Edit'], key='mode') == 'Edit'
 
 with conn.session as session, table:
-    # Initialize database:
     session.execute(text(
         '''
         CREATE TABLE IF NOT EXISTS milk
         (
             id         INTEGER PRIMARY KEY AUTOINCREMENT,
             created_on DATE    NOT NULL,
+            created_by VARCHAR,
             item       VARCHAR NOT NULL,
             selected   BOOLEAN
         );
@@ -77,7 +51,7 @@ with conn.session as session, table:
     session.commit()
 
     items = pd.read_sql_query('''
-                              SELECT m1.id, m1.selected, m1.item
+                              SELECT m1.id, m1.selected, m1.item, m1.created_by
                               FROM milk m1
                               JOIN (SELECT MAX(created_on) AS created_on FROM milk) AS m2
                                 ON m1.created_on = m2.created_on
@@ -85,24 +59,45 @@ with conn.session as session, table:
                               session.connection())
     items['selected'] = items['selected'].astype(bool)
 
-    updated_df = st.data_editor(
-        disabled=not edit, key=f'editor_df',
-        data=items if edit else items[items['selected'] == True],
-        column_order=(['selected'] if edit else []) + ['item'],
-        hide_index=True, num_rows='dynamic' if edit else 'fixed', width='content', height=500,
-        column_config={
-            'id': st.column_config.NumberColumn(disabled=True, width=1),
-            'selected': st.column_config.CheckboxColumn(disabled=False, width=85, default=False),
-            'item': st.column_config.TextColumn(disabled=False, width='medium', validate="^\\S+.*$", required=True)},
-    )
-    if edit:
-        changes = items.merge(updated_df, on=['id'], how='outer').query('selected_x != selected_y | item_x != item_y')
+updated_df = st.data_editor(
+    key=f'editor_df',
+    disabled=not edit,
+    data=(items if edit else items[items['selected'] == True]).reset_index(),
+    column_order=(['selected'] if edit else []) + ['item'],
+    hide_index=edit,
+    num_rows='dynamic' if edit else 'fixed',
+    width='content',
+    height=500,
+    column_config={
+        'id': st.column_config.NumberColumn(disabled=True, width=1),
+        'selected': st.column_config.CheckboxColumn(disabled=False, width=85, default=False),
+        'item': st.column_config.TextColumn(disabled=False, width='medium', validate="^\\S+.*$", required=True)})
 
-        with save:
-            if floating_button(f'Save ({len(changes)})', disabled=len(changes) == 0,
-                               type='primary', icon=':material/save:'):
-                on_rows_change(session)
-                st.rerun()
+if edit:
+    changes = items.merge(updated_df, on=['id'], how='outer').query('selected_x != selected_y | item_x != item_y')
+
+    with save:
+        if floating_button(f'Save ({len(changes)})', disabled=len(changes) == 0,
+                           type='primary', icon=':material/save:'):
+            # Apply row attribution:
+            updated_df.loc[updated_df['id'].isin(changes['id'].unique()), 'created_by'] = \
+                st.context.headers.get(os.environ.get('USER_HEADER', 'X-Auth-Request-Email'))
+
+            with conn.session as session:
+                session.execute(text('DELETE FROM milk WHERE created_on >= :today'), {'today': today})
+                rows = [{
+                    'selected': 1 if r.get('selected') else 0,
+                    'item': r.get('item'),
+                    'created_on': today,
+                    'created_by': r.get('created_by')
+                } for _, r in updated_df.iterrows()]
+                session.execute(
+                    text('INSERT INTO milk (created_on, selected, item, created_by) '
+                         'VALUES (:created_on, :selected, :item, :created_by)'),
+                    rows)
+                session.commit()
+
+            st.rerun()
 
 
 with bottom():
